bb0f248f6fffe44d575ef8201c3f9768bcdba4bc
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-07 23:39:31 -0300

    Create aula1.py
    nesse commit fiz a aula 1

---
+++ aula1.py
@@ -0,0 +1,20 @@
+import pygame
+
+def main():
+    pygame.init()
+    pygame.display.set_mode([300,300])
+    pygame.display.set_caption("meu jogo")
+    
+    sair == False
+
+    while sair != True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sair == True
+
+    pygame.quit()
+
+
+
+
+main()
