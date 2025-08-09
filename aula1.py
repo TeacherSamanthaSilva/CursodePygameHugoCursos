884e396cb4152f1403d36ca508349d26b53579f0
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-09 16:16:46 -0300

    Update aula1.py
    nesse commit eu corrigi o código da aula 1 que não estava funcionando

--- aula1.py
+++ aula1.py
@@ -1,20 +1,41 @@
-import pygame
+import pygame 
 
-def main():
-    pygame.init()
-    pygame.display.set_mode([300,300])
-    pygame.display.set_caption("meu jogo")
-    
-    sair == False
+#Inicializa o pygame
+pygame.init()
 
-    while sair != True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sair == True
+#define o tamanho da tela
+largura = 800
+altura = 600
 
-    pygame.quit()
+#cria a tela
+tela = pygame.display.set_mode(largura,altura)
+pygame.display.set_caption("Meu primeiro jogo")
 
+#controle do loop principal
+rodando = True
 
+while rodando:
+    #loop de eventos
+    #criando o evento de sair
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
 
+#cor da tela de fundo em RGB
+tela.fill(50,100,200)
 
-main()
+#atualiza a tela
+pygame,display.flip()
+
+#finaliza o jogo
+pygame.quit()
+
+""" pygame.event.get() → pega todos os eventos (como clicar no X da janela, pressionar teclas, mover o mouse, etc.).
+
+if evento.type == pygame.QUIT: → verifica se o evento é o de fechar a janela.
+
+rodando = False → encerra o loop principal e fecha o programa.
+
+tela.fill() → preenche a tela com uma cor de fundo.
+
+pygame.display.flip() → atualiza a tela com o que foi desenhado. """
